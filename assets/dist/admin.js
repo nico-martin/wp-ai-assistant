54c0b8effe03f79fc37e8967c3b196de0cd07bdb
Nico Martin <mail@nico.dev>
2024-11-16 11:02:25 +0100

    added admin JS

---
+++ assets/dist/admin.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+/*!***********************************!*\
+  !*** ./assets/src/admin/index.ts ***!
+  \***********************************/
+console.log('Hello from the admin side!');
+
+/******/ })()
+;
+//# sourceMappingURL=admin.js.map
